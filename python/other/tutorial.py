9061b52ffdba1b0c93e0bd4ec36a07b6ec3ff082
soy-curd <soycurd1@gmail.com>
2016-06-05 02:53:16 +0900

    Add itertools examples.

--- python/other/tutorial.py
+++ python/other/tutorial.py
@@ -1,5 +1,6 @@
 import itertools
 
+# itertools
 itr1 = itertools.count(5)
 for i in itr1:
     print(i)
@@ -42,3 +43,21 @@ tee1, tee2, = itertools.tee(ls)
 [print(i) for i in tee1]
 [print(i) for i in tee1]
 [print(i) for i in tee2]
+
+itr8 = itertools.zip_longest("ABCDE", "xyz", fillvalue="-")
+[print(i) for i in itr8]
+
+itr9 = itertools.product("ABCDE", "xyz")
+[print(i) for i in itr9]
+
+itr10 = itertools.permutations([1, 2, 3], 2)
+[print(i) for i in itr10]
+
+itr11 = itertools.combinations([1, 2, 3, 4], 3)
+[print(i) for i in itr11]
+
+itr12 = itertools.combinations_with_replacement([1, 2, 3, 4], 3)
+[print(i) for i in itr12]
+
+itr13 = itertools.accumulate([1, 2, 3, 4, 5, 6])
+[print(i) for i in itr13]
