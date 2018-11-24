b98dabb5812c6e8467de6d625f544180b185cf3a
soy-curd <soycurd1@gmail.com>
2018-11-24 14:47:52 +0900

    add short path

--- c/ari/short_path.cpp
+++ c/ari/short_path.cpp
@@ -1,25 +1,51 @@
-#include <algorithm>
-#include <cstdio>
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A&lang=jp
+#include <iostream>
+#include <string>
 using namespace std;
-const int MAX_N = 100;
 
-int main() {
-  // int n = 5;
-  // int a[MAX_N] = {2, 3, 4, 5, 10};
-  int n = 4;
-  int a[MAX_N] = {4, 5, 10, 20};
-  int *end = a + sizeof(a) / sizeof(a[0]);
-  int ans = 0;
-  sort(a, std::end(a), [](int x, int y) -> int { return (x > y); });
-  int ans_1 = 0;
-  int ans_2 = 0;
-  int ans_3 = 0;
-  for (int i = 0; i < n - 2; i++) {
-    if (a[i] < a[i + 1] + a[i + 2]) {
-      ans = a[i] + a[i + 1] + a[i + 2];
+const int MAX_V = 100000;
+const int MAX_D = 10000;
+const int MAX_E = 500000;
+const int INF = 999999999;
+int V, E, R;
+
+struct edge {
+  int from, to, cost;
+};
+edge es[MAX_E];
+int d[MAX_V];
+
+void solve() {
+  for (int i = 0; i < V; i++) {
+    d[i] = INF;
+  }
+  d[R] = 0; // 初期化
+  while (true) {
+    bool update = false;
+    for (int i = 0; i < E; i++) {
+      edge e = es[i];
+      if (d[e.from] != INF && d[e.to] > d[e.from] + e.cost) {
+        d[e.to] = d[e.from] + e.cost;
+        update = true;
+      }
+    }
+    if (!update) {
       break;
     }
   }
+  for (int i = 0; i < V; i++) {
+    cout << ((d[i] == INF) ? "INF" : to_string(d[i])) << endl;
+  }
+}
 
-  printf("%d\n", ans);
+int main() {
+  cin >> V >> E >> R;
+  cin.ignore();
+  for (int i = 0; i < E; i++) {
+    int from, to, cost;
+    cin >> from >> to >> cost;
+    cin.ignore();
+    es[i] = edge{from, to, cost};
+  }
+  solve();
 }
